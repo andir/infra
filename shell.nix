7d529c2ef59e1e1e8eb6e66527d187d75e13fd38
Andreas Rammhold <andreas@rammhold.de>
2019-09-17 21:59:49 +0200

    Add command to eval server configurations without having to build them

--- shell.nix
+++ shell.nix
@@ -1,5 +1,10 @@
 let
   pkgs = import ./nix;
+
+  evalServers = pkgs.writeScriptBin "eval-servers" ''
+    nix eval '(map (n: n.config.system.build.toplevel) (builtins.attrValues ((import ${pkgs.morph.lib}/eval-machines.nix) { networkExpr = ./config/servers.nix; }).nodes))'
+  '';
+
 in pkgs.mkShell {
-  buildInputs = with pkgs; [ niv morph ];
+  buildInputs = with pkgs; [ niv morph hcloud evalServers ];
 }
