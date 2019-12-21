8b90f9a8f0435a0fbacd564e44367ceef63951ad
Andreas Rammhold <andreas@rammhold.de>
2019-12-21 22:59:28 +0100

    Add custom `modules` repo to dependencies and to VMs

--- config/lib.nix
+++ config/lib.nix
@@ -1,8 +1,11 @@
+let
+  extraModules = (import (import ../nix/sources.nix).modules).all;
+in
 {
   # Inject my custom modules into the machine and set some common things.
   mkMachine = config: {
     imports = [
       config
-    ];
+    ] ++ extraModules;
   };
 }
