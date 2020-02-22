fb588e2c90bdc593b9711f9386236c5456e6aa79
Andreas Rammhold <andreas@rammhold.de>
2020-02-22 18:58:14 +0100

    Move local dns resolvers to 127.0.1.53
    This makes it easier to run public DNS server instances without having
    to on a per-servier basis having to move the local (caching) resolver.

--- config/profiles/dns.nix
+++ config/profiles/dns.nix
@@ -2,8 +2,18 @@
 {
   networking.nameservers = [ "::1" ];
   services.resolved.enable = false;
+  networking.resolvconf = {
+    useLocalResolver = false;
+    extraConfig = "name_servers='127.0.1.53'";
+  };
+  services.kresd = {
+    interfaces = lib.mkDefault [
+      "127.0.1.53"
+    ];
+  };
   services.unbound = {
     enable = lib.mkDefault true;
+    interfaces = [ "127.0.1.53" ];
     allowedAccess = [
       "::1/128"
       "127.0.0.0/8"
