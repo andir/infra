42cfe8278cd99a903ed1801b72a46495aecf56f6
Andreas Rammhold <andreas@rammhold.de>
2020-03-27 21:42:49 +0100

    mail: create btrfs snapshots before the backup of mails
    In th past backups would randomly fail when something in the maildir
    was moved. By creating a proper snapshot before backing them solved
    this.

--- config/servers/mail/backup.nix
+++ config/servers/mail/backup.nix
@@ -1,8 +1,8 @@
-{ config, ... }:
+{ pkgs, config, ... }:
 {
   services.borgbackup.jobs = {
     "mail" = {
-      inherit (config.h4ck.backup) paths;
+      paths = map (path: if path == config.mailserver.mailDirectory then "/data/snapshots/mails" else path) config.h4ck.backup.paths;
       startAt = "hourly";
       compression = "lz4";
       repo = "borg@epsilon.rammhold.de:/home/borg/backups/mail.h4ck.space";
@@ -10,6 +10,16 @@
         mode = "repokey";
         passCommand = "cat /var/lib/secrets/borg.password";
       };
+
+      preHook = ''
+        set -e
+        ${pkgs.btrfs-progs}/bin/btrfs subvolume snapshot -r /data/mails /data/snapshots/mails
+      '';
+      postHook = ''
+        set -e
+        ${pkgs.btrfs-progs}/bin/btrfs subvolume delete /data/snapshots/mails
+      '';
     };
   };
+  systemd.services.borgbackup-job-mail.serviceConfig.ReadWritePaths = [ "/data/snapshots" ];
 }
