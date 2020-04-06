9ee6990a5e45e7a634b27dfd3667499e1c993980
Andreas Rammhold <andreas@rammhold.de>
2020-04-06 18:04:33 +0200

    Add my binary cache to all servers

--- config/profiles/base/nix.nix
+++ config/profiles/base/nix.nix
@@ -6,5 +6,13 @@
       automatic = lib.mkDefault true;
       options = lib.mkDefault "--delete-older-than 14d";
     };
+    binaryCaches = [
+      "https://cache.nixos.org"
+      "https://cache.h4ck.space"
+    ];
+
+    binaryCachePublicKeys = [
+      "zeta:9zm3cHRlqz3T9HnRsodtQGGqHOLDAiB+8d0kOKnFI0M="
+    ];
   };
 }
