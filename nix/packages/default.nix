7ff5715d9f2843a4b6167f549175241644310017
Andreas Rammhold <andreas@rammhold.de>
2020-04-14 21:26:38 +0200

    bird: apply babel onlink patch

--- nix/packages/default.nix
+++ nix/packages/default.nix
@@ -12,4 +12,13 @@ self: super: {
       sha256 = "1franflr7xci8zdcd5qmqxxalnimjpz2yyhpa51la5d16mnmch9r";
     };
   };
+
+  bird2 = super.bird2.overrideAttrs ({ patches ? [], ... }: {
+    patches = patches ++ [
+      (super.fetchpatch {
+        url = "https://github.com/openwrt-routing/packages/raw/37f8c509e03de355a3d0f9b6d62837c43750f98b/bird2/patches/0001-babel-Set-onlink-flag-for-IPv4-routes-with-unreachab.patch";
+        sha256 = "08x5awza514r6745l5cg0mwlh7pcbxsws9y8hmjmk831wdqgjlln";
+      })
+    ];
+  });
 }
