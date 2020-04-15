366a4938e60483614b83e40461a8d914a75ea38a
Andreas Rammhold <andreas@rammhold.de>
2020-04-16 01:01:05 +0200

    bertha: update to systemd v245

--- config/servers/bertha/nixpkgs.nix
+++ config/servers/bertha/nixpkgs.nix
@@ -1,14 +1,18 @@
 let
-  inherit (import (../../../nix/sources.nix)) bertha-nixpkgs;
+  sources = import (../../../nix/sources.nix);
+  inherit (sources) bertha-nixpkgs;
 in
 {
   nixpkgs.pkgs = import bertha-nixpkgs {
     overlays = [
-      (import ../../../nix/packages/default.nix)
+      (import ../../../nix/packages/default.nix { inherit sources; })
     ];
   };
   disabledModules = [ "system/boot/networkd.nix" ];
   imports = [
     (bertha-nixpkgs + "/nixos/modules/system/boot/networkd.nix")
   ];
+
+  # fixup the broken mdmonitor unit by masking it
+  systemd.units."mdmonitor.service".enable = false;
 }
