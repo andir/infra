51c4a5ac35bf3ac3020a60cd1222f52412bd6976
Andreas Rammhold <andreas@rammhold.de>
2020-05-09 12:48:30 +0200

    bertha: disable the unifi controller nginx proxying for now

--- config/servers/bertha/unifi.nix
+++ config/servers/bertha/unifi.nix
@@ -6,28 +6,29 @@
   };
 
   services.nginx = {
-    enable = true;
-    virtualHosts."unifi.epsilon.rammhold.de" = {
-      locations."/" = {
-        proxyPass = "https://";
-        proxyWebsockets = true;
-        extraConfig = let
-          networks = lib.flatten (
-            map (
-              iface:
-                (
-                  map
-                    (addr: addr.address + "/${toString addr.prefixLength}")
-                    (iface.v4Addresses ++ iface.v6Addresses)
-                )
-            ) config.router.downstreamInterfaces
-          );
-        in
-          ''
-            ${lib.concatMapStringsSep "\n" (addr: "allow ${addr};") networks}
-            deny   all;
-          '';
-      };
-    };
+    # disabled since unifi seems to request logins for unknown reason..
+    #enable = true;
+    #virtualHosts."unifi.epsilon.rammhold.de" = {
+    #  locations."/" = {
+    #    proxyPass = "https://localhost:8443";
+    #    proxyWebsockets = true;
+    #    extraConfig = let
+    #      networks = lib.flatten (
+    #        map (
+    #          iface:
+    #            (
+    #              map
+    #                (addr: addr.address + "/${toString addr.prefixLength}")
+    #                (iface.v4Addresses ++ iface.v6Addresses)
+    #            )
+    #        ) config.router.downstreamInterfaces
+    #      );
+    #    in
+    #      ''
+    #        ${lib.concatMapStringsSep "\n" (addr: "allow ${addr};") networks}
+    #        deny   all;
+    #      '';
+    #  };
+    #};
   };
 }
