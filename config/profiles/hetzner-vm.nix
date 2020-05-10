254fd442b6be6a54e48175ba4804ce6382ba81f4
Andreas Rammhold <andreas@rammhold.de>
2020-05-10 20:20:20 +0200

    profiles/hetzner-vm: add static route to next hop

--- config/profiles/hetzner-vm.nix
+++ config/profiles/hetzner-vm.nix
@@ -23,6 +23,15 @@
     networking.ipv6 = {
       gateway = "fe80::1";
     };
-
   };
+
+  # ensure that all the hetzner hosts have a proper route to their default
+  # gateway. If we don't add this some blackholes might make it impossible to
+  # reach the gateway since on-link gateways are still resolved through the
+  # routing table.
+  systemd.network.networks."10-uplink".routes = [
+    {
+      routeConfig.Destination = "172.31.1.1/32";
+    }
+  ];
 }
