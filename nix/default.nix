3b2365e7a006b150d131c7cf75ee1d2174cc5a7e
Andreas Rammhold <andreas@rammhold.de>
2020-05-19 22:17:33 +0200

    Update dependencies

--- nix/default.nix
+++ nix/default.nix
@@ -1,11 +1,16 @@
 let
   sources = import ./sources.nix;
+
+  # morph needs a newer buildGoPackage since they track nixpkgs unstable
+  unstable = import sources.nixpkgs-unstable {};
+
   overlays = [
     (_: pkgs: { inherit (import sources.niv {}) niv; })
-    (_: pkgs: { morph = pkgs.callPackage (sources.morph + "/nix-packaging") {}; })
+    (_: _: { morph = unstable.callPackage (sources.morph + "/nix-packaging") {}; })
     (_: _: { c3schedule = import sources.c3schedule {}; })
     (import ./packages { inherit sources; })
     (_: _: { nix-pre-commit-hooks = import (sources."pre-commit-hooks.nix"); })
   ];
+
 in
 import sources.nixpkgs { inherit overlays; config = {}; }
