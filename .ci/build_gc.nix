d8ce95a128a48c4d85432e94c8f9c708f15775dd
Andreas Rammhold <andreas@rammhold.de>
2020-05-25 12:36:21 +0200

    ci: also store niv sources in the gcroot

--- .ci/build_gc.nix
+++ .ci/build_gc.nix
@@ -2,8 +2,11 @@
 let
   pkgs = import ../nix;
   shell = import ../shell.nix;
-in pkgs.runCommand "shell-gc-root" {} ''
+  sources = import ../nix/sources.nix;
+in
+pkgs.runCommand "shell-gc-root" {} ''
   mkdir $out
   ${pkgs.lib.concatMapStringsSep "\n" (n: "ln -s ${n} $out") shell.buildInputs}
+  ${pkgs.lib.concatMapStringsSep "\n" (n: "ln -s ${n} $out") (pkgs.lib.filter (n: builtins.typeOf n != "lambda") (pkgs.lib.attrValues sources))}
   ln -s ${morph} $out
 ''
