b4387eb7596c0ecb82d0ef633fb3edb141942a99
Andreas Rammhold <andreas@rammhold.de>
2020-07-11 20:04:01 +0200

    gallery: proxy photoprism through NGINX and enable TLS

--- config/servers/gallery/default.nix
+++ config/servers/gallery/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   environment.systemPackages = [ pkgs.photoprism ];
   imports = [
@@ -16,6 +16,23 @@
       "159.69.192.67/32"
       "2a01:4f8:c2c:2ae2::/128"
     ];
+  };
+
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
+
+  services.nginx = {
+    enable = true;
+    virtualHosts."gallery.rammhold.de" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://localhost:${toString config.h4ck.photoprism.port}";
+        proxyWebsockets = true;
+        extraConfig = ''
+          client_max_body_size 100M;
+        '';
+      };
+    };
   };
 
   h4ck.photoprism.enable = true;
