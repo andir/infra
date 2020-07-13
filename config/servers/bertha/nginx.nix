baea0aaa7b6f41b2f16a8b5bfeb27daaedaf37b8
Andreas Rammhold <andreas@rammhold.de>
2020-07-13 13:18:44 +0200

    bertha: serve an empty webroot instead of the NGINX default page

--- config/servers/bertha/nginx.nix
+++ config/servers/bertha/nginx.nix
@@ -1,9 +1,14 @@
+{ pkgs, ... }:
 {
   services.nginx = {
     enable = true;
     virtualHosts."epsilon.rammhold.de" = {
       forceSSL = true;
       enableACME = true;
+      root = pkgs.symlinkJoin {
+        name = "webroot";
+        paths = [];
+      };
       locations."/weechat" = {
         proxyPass = "https://[fd21:a07e:735e:ff01:ae1f:6bff:fe45:be15]:9001";
         proxyWebsockets = true;
