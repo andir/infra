d8c200cf1d8963735d8af3eae3aa8c57439754ae
Andreas Rammhold <andreas@rammhold.de>
2020-07-13 22:11:00 +0200

    ci: conver the passsed in path to a store path

--- .ci/build_gc.nix
+++ .ci/build_gc.nix
@@ -8,5 +8,5 @@ pkgs.runCommand "shell-gc-root" {} ''
   mkdir $out
   ${pkgs.lib.concatMapStringsSep "\n" (n: "ln -s ${n} $out") shell.buildInputs}
   ${pkgs.lib.concatMapStringsSep "\n" (n: "ln -s ${n} $out") (pkgs.lib.filter (n: builtins.typeOf n != "lambda") (pkgs.lib.attrValues sources))}
-  ln -s ${morph} $out
+  ln -s ${builtins.storePath morph} $out
 ''
