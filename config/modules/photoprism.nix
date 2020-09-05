7dc51f24ea263fc77fb740237bfccb6712967019
Andreas Rammhold <andreas@rammhold.de>
2020-09-06 00:13:16 +0200

    photoprims: support different storage directory

--- config/modules/photoprism.nix
+++ config/modules/photoprism.nix
@@ -16,10 +16,19 @@ in
       type = lib.types.port;
       default = 2342;
     };
+    storagePath = lib.mkOption {
+      type = lib.types.string;
+      default = "/var/lib/photoprism";
+    };
   };
 
   config = lib.mkIf cfg.enable {
-    h4ck.backup.paths = [ "/var/lib/photoprism" "/var/lib/private/photoprism" ];
+
+    users.users.photoprism = {
+      createHome = false;
+    };
+
+    h4ck.backup.paths = [ "${cfg.storagePath}" ] ++ lib.optional (cfg.storagePath == "/var/lib/photoprism") "/var/lib/private/photoprism";
     systemd.services.photoprism = {
       wantedBy = [ "multi-user.target" ];
       path = with pkgs; [
@@ -34,15 +43,15 @@ in
         lib.mapAttrs' (n: v: lib.nameValuePair "PHOTOPRISM_${n}" (toString v)) {
           DEBUG = 1;
           DATABASE_DRIVER = "sqlite";
-          DATABASE_DSN = "/var/lib/photoprism/photoprism.sqlite";
+          DATABASE_DSN = "${cfg.storagePath}/photoprism.sqlite";
           DATABASE_CONNS = 256;
           ASSETS_PATH = "${pkgs.photoprism.assets}";
           STORAGE_PATH = "/var/cache/photoprism/";
-          ORIGINALS_PATH = "/var/lib/photoprism/originals";
+          ORIGINALS_PATH = "${cfg.storagePath}/originals";
           SIDECAR_JSON = 1;
-          SIDECAR_PATH = "/var/lib/photoprism/sidecar";
+          SIDECAR_PATH = "${cfg.storagePath}/sidecar";
           CACHE_PATH = "/var/cache/photoprism/cache";
-          IMPORT_PATH = "/var/lib/photoprism/import";
+          IMPORT_PATH = "${cfg.storagePath}/import";
           ADMIN_PASSWORD = "admin"; # initial admin passsword, overriden through the settings UI?!
           TEMP_PATH = "/tmp";
           HTTP_PORT = cfg.port;
@@ -55,7 +64,7 @@ in
         exec ${pkgs.photoprism}/bin/photoprism start
       '';
       serviceConfig = {
-        DynamicUser = true;
+        User = "photoprism";
         RuntimeDirectory = "photoprism";
         CacheDirectory = "photoprism";
         StateDirectory = "photoprism";
