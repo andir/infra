5e6a4373311bba642de406713a5d6fc759e71bc3
Andreas Rammhold <andreas@rammhold.de>
2020-09-12 11:20:40 +0200

    webserver: rotate logfiles daily

--- config/profiles/webserver.nix
+++ config/profiles/webserver.nix
@@ -1,8 +1,39 @@
-{ lib, ... }:
+{ lib, config, pkgs, ... }:
 {
   services.nginx = {
     recommendedGzipSettings = lib.mkDefault true;
     recommendedOptimisation = lib.mkDefault true;
     recommendedTlsSettings = lib.mkDefault true;
+  };
+
+  services.logrotate = {
+    enable = config.services.nginx.enable;
+    # For >= 20.09
+    #paths = lib.mkIf config.nginx.enable {
+    #  nginx = {
+    #    path = "/var/spool/nginx/logs";
+    #    user = config.services.nginx.user;
+    #    group = config.services.nginx.group;
+    #    keep = 30;
+    #    frequency = "daily";
+    #    extraConfig = ''
+    #      compress
+    #    '';
+    #  };
+    #};
+    config = ''
+      "/var/spool/nginx/logs/*.log" {
+        su ${config.services.nginx.user} ${config.services.nginx.group}
+        daily
+        rotate 30
+        delaycompress
+        compress
+        missingok
+        notifempty
+        postrotate
+          ${pkgs.systemd}/bin/systemctl reload nginx
+        endscript
+      }
+    '';
   };
 }
