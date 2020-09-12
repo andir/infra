d2049edf43e6bc7ec61cfc4a13a3a64175cddfd8
Andreas Rammhold <andreas@rammhold.de>
2020-09-12 11:20:10 +0200

    roundcube: fix sending mails via STARTTLS

--- config/servers/mail/roundcube.nix
+++ config/servers/mail/roundcube.nix
@@ -9,6 +9,16 @@
       "managesieve"
       "acl"
     ];
+    extraConfig = ''
+      $config['smtp_server'] = 'tls://mx.h4ck.space';
+      $config['smtp_conn_options'] = array(
+        'ssl'         => array(
+          'verify_peer'  => true,
+          'verify_depth' => 3,
+          'cafile'       => '/etc/ssl/certs/ca-bundle.crt',
+        ),
+      );
+    '';
   };
   services.nginx.virtualHosts."webmail.h4ck.space" = {
     serverAliases = [ "mail.jh4all.de" "webmail.kack.it" ];
