1117a4ecfd583ce68d014c8114df9fc278847c8e
Andreas Rammhold <andreas@rammhold.de>
2020-10-06 23:12:34 +0200

    acme: update comment about ordering, link to PR

--- config/modules/base.nix
+++ config/modules/base.nix
@@ -9,6 +9,7 @@
     h4ck.fqdn = config.networking.hostName + (lib.optionalString (config.networking.domain != null) ".${config.networking.domain}");
 
     # FIXME: upstream this change if it turns out to really fix the unbound being unavailable during acme run
+    # Can be removed once https://github.com/NixOS/nixpkgs/pull/99901 hits nixos-20.09
     systemd.services.acme-fixperms.after = lib.mkIf (config.systemd.services ? acme-fixperms) [ "nss-lookup.target" ];
   };
 }
