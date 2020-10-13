31e8c70d2d9b4850d65e2b15ac958caef731356e
Andreas Rammhold <andreas@rammhold.de>
2020-10-14 00:47:06 +0200

    Update dependencies

--- nix/packages/prometheus-xmpp-alerts.nix
+++ nix/packages/prometheus-xmpp-alerts.nix
@@ -9,8 +9,8 @@ python3.pkgs.buildPythonApplication {
   src = fetchFromGitHub {
     owner = "andir";
     repo = pname;
-    rev = "e85f483b058800fb90dc338eb84d8860c9cb4994";
-    sha256 = "0qczp3mpsyhcvf62jqh8n3xcc1vjp53gic7n7mg1x97jjl5k3jmh";
+    rev = "500a316a83b5d72c490cb62c3e7f5aa5efc7f5bf";
+    sha256 = "0lphw62g7jmgjhdxng0jfw8pb5dzc71qv5s10dxbswjhki003zpn";
   };
 
   propagatedBuildInputs = with python3.pkgs; [
