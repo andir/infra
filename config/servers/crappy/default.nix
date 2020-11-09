247af2b3abb15975c306ba79ca25b46a1584c09f
Andreas Rammhold <andreas@rammhold.de>
2020-11-09 17:29:50 +0100

    crappy: move hardware specifics & add pulseaudio service

--- config/servers/crappy/default.nix
+++ config/servers/crappy/default.nix
@@ -2,6 +2,7 @@
 {
   imports = [
     ../../profiles/server.nix
+    ./hardware.nix
   ];
 
   deployment = {
@@ -11,45 +12,15 @@
   };
   h4ck.monitoring.targetHost = config.deployment.targetHost;
 
-
-  # ensure we are building for the right architecture
-  nixpkgs.system = "aarch64-linux";
-
-  # NixOS wants to enable GRUB by default
-  boot.loader.grub.enable = false;
-  # Enables the generation of /boot/extlinux/extlinux.conf
-  boot.loader.generic-extlinux-compatible.enable = true;
-
-  #  !!! If your board is a Raspberry Pi 3, select not latest (5.8 at the time)
-  #  !!! as it is currently broken (see https://github.com/NixOS/nixpkgs/issues/97064)
-  boot.kernelPackages = pkgs.linuxPackages;
-
-  # !!! Needed for the virtual console to work on the RPi 3, as the default of 16M doesn't seem to be enough.
-  # If X.org behaves weirdly (I only saw the cursor) then try increasing this to 256M.
-  # On a Raspberry Pi 4 with 4 GB, you should either disable this parameter or
-  # increase to at least 64M if you want the USB ports to work.
-  boot.kernelParams = [ "cma=32M" ];
-
-  # File systems configuration for using the installer's partition layout
-  fileSystems = {
-    "/" = {
-      device = "/dev/disk/by-label/NIXOS_SD";
-      fsType = "ext4";
+  hardware.pulseaudio = {
+    enable = true;
+    systemWide = true;
+    package = pkgs.pulseaudioFull;
+    zeroconf.publish.enable = true;
+    tcp = {
+      enable = true;
+      anonymousClients.allowAll = true;
     };
   };
 
-  # !!! Adding a swap file is optional, but strongly recommended!
-  swapDevices = [{ device = "/swapfile"; size = 1024; }];
-
-  systemd.network = {
-    networks = {
-      "0-default" = {
-        matchConfig.Name = "eth0";
-        networkConfig = {
-          DHCP = "yes";
-          IPv6AcceptRA = true;
-        };
-      };
-    };
-  };
 }
