3f5e71210c10e62d624d8b409a17e008deddecb6
Andreas Rammhold <andreas@rammhold.de>
2020-11-16 16:32:20 +0100

    ci: fixup nixpkgs changes

--- .ci/build_gc.nix
+++ .ci/build_gc.nix
@@ -1,10 +1,10 @@
 { morph }:
 let
-  pkgs = import ../nix;
+  pkgs = import ../nix { };
   shell = import ../shell.nix;
   sources = import ../nix/sources.nix;
 in
-pkgs.runCommand "shell-gc-root" {} ''
+pkgs.runCommand "shell-gc-root" { } ''
   mkdir $out
   ${pkgs.lib.concatMapStringsSep "\n" (n: "ln -s ${n} $out") shell.buildInputs}
   ${pkgs.lib.concatMapStringsSep "\n" (n: "ln -s ${n} $out") (pkgs.lib.filter (n: builtins.typeOf n != "lambda") (pkgs.lib.attrValues sources))}
