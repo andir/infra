9845802cc4580d03af0c9ccb2da4bdd52a4a3ff7
Andreas Rammhold <andreas@rammhold.de>
2021-01-21 11:36:10 +0100

    mon: monitor one host of litchi

--- config/modules/monitoring.nix
+++ config/modules/monitoring.nix
@@ -162,6 +162,7 @@ in
         "ixfr3-bb1.nynex.de" = { };
         "usi-corea.nynex.de" = { };
         "ws.flokli.de" = { };
+        "magrathea.litschi.xyz" = { };
       };
     };
 
@@ -180,6 +181,7 @@ in
         "eqixfr5-bb2.nynex.de" = { };
         "ixfr3-bb1.nynex.de" = { };
         "ws.flokli.de" = { };
+        "magrathea.litschi.xyz" = { };
       };
     };
 
