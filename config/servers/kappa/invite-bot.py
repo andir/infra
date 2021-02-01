01b4a58dd8b9a17f6ec6fa1919135772615c0d36
Andreas Rammhold <andreas@rammhold.de>
2021-02-01 10:33:18 +0100

    Revert "Update conversejs" since it doesn't really work anymore
    The login is stuck at some connection error that JS is throwing on the
    new version. There is an upstream bug for this with some workarounds
    that didn't work for me.
    
    This reverts commit 74f765a83e7e1d577648e12d0fa7f40e0cc43d1b.

--- config/servers/kappa/invite-bot.py
+++ config/servers/kappa/invite-bot.py
@@ -20,7 +20,7 @@ class TestBot(irc.bot.SingleServerIRCBot):
 
     def on_pubmsg(self, c ,e):
         if e.arguments[0] == '!!invite':
-            nick = re.sub('[^a-zA-Z0-9]', '_', e.source.nick)
+            nick = re.sub('[^a-zA-z0-9]', '_', e.source.nick)
             self.connection.notice(self.channel, "does some magic...")
             output = subprocess.check_output(["machinectl", "shell", "wan-party", "/bin/sh", "-c", f'sudo -u tinc.wan-party tinc.wan-party invite {nick}'])
             for line in output.splitlines():
