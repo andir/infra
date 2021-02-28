b894794b7f972f7e2d280734d54137346753bad3
Andreas Rammhold <andreas@rammhold.de>
2021-02-28 14:08:44 +0100

    Add the hass stuff that I use for debugging right now

--- config/servers/crappy/hass.nix
+++ config/servers/crappy/hass.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }:
 {
+  # for updating the conbee II firmware via the deconz image:
+  # mkdir /tmp/test
+  # docker run  --rm --privileged  -v /tmp/test:/root/.local/share/dresden-elektronik/deCONZ --device=/dev/ttyACM0 --entrypoint /firmware-update.sh -it marthoc/deconz
+  virtualisation.docker.enable = true;
   services.home-assistant = {
     enable = true;
     autoExtraComponents = false;
@@ -16,6 +20,24 @@
     };
 
     config = {
+      logger = {
+        default = "info";
+        # logs = {
+        #   "homeassistant.core" = "debug";
+        #   "homeassistant.components.zha" = "debug";
+        #   "bellows.zigbee.application" = "debug";
+        #   "bellows.ezsp" = "debug";
+        #   zigpy = "debug";
+        #   zigpy_cc = "debug";
+        #   "zigpy_deconz.zigbee.application" = "debug";
+        #   "zigpy_deconz.api" = "debug";
+        #   "zigpy_xbee.zigbee.application" = "debug";
+        #   "zigpy_xbee.api" = "debug";
+        #   zigpy_zigate = "debug";
+        #   zigpy_znp = "debug";
+        #   zhaquirks = "debug";
+        # };
+      };
       homeassistant = {
         name = "Home";
         latitude = "49.872707";
@@ -43,6 +65,7 @@
       zha = {
         database_path = "/var/lib/hass/zha.database";
         zigpy_config = { };
+        enable_quirks = true;
       };
       prometheus = {
         namespace = "epsilon";
@@ -65,6 +88,59 @@
                 "sensor.lumi_lumi_weather_189d7f05_temperature"
                 "sensor.lumi_lumi_weather_189d7f05_power"
               ];
+            }
+
+            {
+              type = "entities";
+              title = "Balcony";
+              entities = [
+                "sensor.lumi_lumi_weather_humidity"
+                "sensor.lumi_lumi_weather_pressure"
+                "sensor.lumi_lumi_weather_temperature"
+                "sensor.lumi_lumi_weather_power"
+              ];
+            }
+
+            {
+              type = "entities";
+              title = "Kitchen";
+              entities = [
+                "sensor.lumi_lumi_weather_75648a05_humidity"
+                "sensor.lumi_lumi_weather_75648a05_pressure"
+                "sensor.lumi_lumi_weather_75648a05_temperature"
+                "sensor.lumi_lumi_weather_75648a05_power"
+              ];
+            }
+            {
+              type = "entities";
+              title = "Bedroom";
+              entities = [
+                "sensor.lumi_lumi_weather_10cc8805_humidity"
+                "sensor.lumi_lumi_weather_10cc8805_pressure"
+                "sensor.lumi_lumi_weather_10cc8805_temperature"
+                "sensor.lumi_lumi_weather_10cc8805_power"
+              ];
+            }
+
+            {
+              type = "entities";
+              title = "Bathroom";
+              entities = [
+                "sensor.lumi_lumi_weather_b6ce8805_humidity"
+                "sensor.lumi_lumi_weather_b6ce8805_pressure"
+                "sensor.lumi_lumi_weather_b6ce8805_temperature"
+                "sensor.lumi_lumi_weather_b6ce8805_power"
+              ];
+            }
+            {
+              type = "entities";
+              title = "Powerplug";
+              entities = [
+                "sensor.lumi_lumi_plug_maeu01_ce40823c_eletrical_measurement"
+                "sensor.lumi_lumi_plug_maeu01_ce40823c_on_off"
+                "sensor.lumi_lumi_plug_maeu01_ce40823c_smartenergy_metering"
+              ];
+
             }
           ];
         }
