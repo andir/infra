fa84139f2ee97f10221953ec64d4baa1a178bc29
Andreas Rammhold <andreas@rammhold.de>
2021-06-02 00:35:23 +0200

    mon: monitor matrix.hackint.org

--- config/servers/mon/prom-external-targets.nix
+++ config/servers/mon/prom-external-targets.nix
@@ -13,4 +13,22 @@
     targetHost = "matrix.hackint.org";
     port = 9100;
   };
+
+  h4ck.monitoring.targets."matrix.hackint.org-synapse" = {
+    targetHost = "matrix.hackint.org";
+    port = 443;
+    job_config = {
+      scheme = "https";
+      metrics_path = "/synapse-metrics";
+    };
+  };
+
+  h4ck.monitoring.targets."matrix.hackint.org-appservice" = {
+    targetHost = "matrix.hackint.org";
+    port = 443;
+    job_config = {
+      scheme = "https";
+      metrics_path = "/appservice-metrics";
+    };
+  };
 }
