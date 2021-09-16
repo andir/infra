b85784d36f3440c6f4587e12d8fd2eb85c9fed91
Andreas Rammhold <andreas@rammhold.de>
2021-09-16 19:13:37 +0200

    kack.it: increase matrix client proxy timeout

--- config/servers/kack-it/synapse.nix
+++ config/servers/kack-it/synapse.nix
@@ -70,7 +70,9 @@
           extraConfig = ''
             proxy_set_header Host $host;
             proxy_set_header X-Forwarded-For $remote_addr;
-            proxy_set_header X-Forwarded-Proto $scheme;'';
+            proxy_set_header X-Forwarded-Proto $scheme;
+            proxy_read_timeout 900s;
+          '';
         };
         #        "/_synapse/" = {
         #          proxyPass = "http://[::1]:8448";
