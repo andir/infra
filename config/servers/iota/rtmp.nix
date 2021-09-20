ca100ab16a057df126899877d248b6ffdbfbe8ee
Andreas Rammhold <andreas@rammhold.de>
2021-09-20 16:27:44 +0200

    iota: update RTMP setup

--- config/servers/iota/rtmp.nix
+++ config/servers/iota/rtmp.nix
@@ -1,4 +1,16 @@
-{ config, pkgs, lib, ... }: {
+{ config, pkgs, lib, ... }:
+let
+  dashjs_src = pkgs.fetchzip {
+    url = "https://github.com/Dash-Industry-Forum/dash.js/archive/refs/tags/v4.0.1.zip";
+    sha256 = "1q1jbrh41h9yviprla88872dfm2w0wmp8iv2gn343biw92wn6zlg";
+  };
+
+  dashjs = pkgs.runCommand "dash.js" { inherit dashjs_src; } ''
+    cp $dashjs_src/dist/dash.all.min.js $out
+  '';
+  fragmentLengthSeconds = 4;
+in
+{
 
   services.nginx = {
     enable = true;
@@ -7,11 +19,22 @@
         rtmp
         dav
         moreheaders
+        lua
       ];
     };
     virtualHosts."jetzt.kack.it" = {
       enableACME = true;
       forceSSL = true;
+      locations."/time".extraConfig = ''
+        client_max_body_size 10k;
+        client_body_buffer_size 10k;
+        default_type "text/plain";
+        content_by_lua_block {
+          ngx.req.read_body()
+          local date = os.date("!%Y-%m-%dT%H:%M:%SZ")
+          ngx.say(date)
+        }
+      '';
       locations."/hls".extraConfig = ''
         # Serve HLS fragments
         types {
@@ -63,10 +86,7 @@
       '';
       locations."= /dash.all.min.js".extraConfig = ''
         default_type "text/javascript";
-        alias ${pkgs.fetchurl {
-          url = "http://cdn.dashjs.org/v3.2.0/dash.all.min.js";
-          sha256 = "16f0b40gdqsnwqi01s5sz9f1q86dwzscgc3m701jd1sczygi481c";
-        }};
+        alias ${dashjs};
       '';
       locations."= /".return = "302 /player";
       locations."= /player".extraConfig = ''
@@ -76,20 +96,71 @@
           <html lang="en">
             <head>
               <meta charset="utf-8">
-              <title>lassulus livestream</title>
+              <title>livestream</title>
+              <style>
+                video {
+                  max-width: 1920px;
+                  max-height: 1080px;
+                  height: 100vh;
+                  width: 100vw;
+                }
+                body {
+                  margin: 0;
+                  padding: 0;
+                  background-color: black;
+                  width: 100%;
+                  height: 100%;
+                  display: flex;
+                  align-teims: center;
+                  justify-content: center;
+                }
+              </style>
             </head>
             <body>
-              <div>
-                <video id="player" controls></video>
-                </video>
-              </div>
               <script src="/dash.all.min.js"></script>
+              <video id="player" controls="true"></video>
+              <div id="debug" style="display: none">
+                <p>WClock: <span id="min"></span>:<span id="sec"></span></p>
+                <p>Delay: <span id="delay"></span></p>
+                <p>Buffer: <span id="buffer"></span></p></p>
+              </div>
+
               <script>
-                (function(){
-                  var url = "/dash/andi.mpd";
-                  var player = dashjs.MediaPlayer().create();
-                  player.initialize(document.querySelector("#player"), url, true);
-                })();
+                var video = document.querySelector('#player');
+                var player = dashjs.MediaPlayer().create();
+                player.clearDefaultUTCTimingSources();
+                player.updateSettings(${builtins.toJSON {
+                  streaming = {
+                    #delay.liveDelayFragmentCount = 2;
+                    #delay.liveDelay = 2;
+                    gaps = {
+                      jumpGaps = false;
+                      jumpLargeGaps = true;
+                      smallGapLimit = 1.5;
+                    };
+                    utcSynchronization = {
+                      defaultTimingSource = {
+                        scheme = "urn:mpeg:dash:utc:http-xsdate:2014";
+                        value = "/time";
+                    };
+                  };
+                };
+                }});
+                player.initialize(video, "/dash/andi.mpd", true);
+
+                setInterval(function() {
+                  var d = new Date();
+                  var seconds = d.getSeconds();
+                  var minutes = d.getMinutes();
+                  document.querySelector("#sec").innerHTML = (seconds < 10 ? '0' : '${""}') + seconds;
+                  document.querySelector("#min").innerHTML = (minutes < 10 ? '0' : '${""}') + minutes;
+
+                  var delay = Math.round((d.getTime() / 1000) - Number(player.timeAsUTC()));
+                  document.querySelector("#delay").innerHTML = delay;
+
+                  var buffer = player.getBufferLength();
+                  document.querySelector("#buffer").innerHTML = buffer;
+                }, 1000);
               </script>
             </body>
           </html>
@@ -112,12 +183,18 @@
 
             hls on;
             hls_path /var/lib/rtmp/tmp/hls;
-            hls_fragment 15;
+            hls_fragment ${toString fragmentLengthSeconds}s;
+            hls_playlist_length ${toString (fragmentLengthSeconds * 4)}s;
 
             dash on;
-            dash_fragment 15;
-            dash_playlist_length 90s;
+            dash_fragment ${toString fragmentLengthSeconds}s;
+            dash_playlist_length ${toString (fragmentLengthSeconds * 4)}s;
             dash_path /var/lib/rtmp/tmp/dash;
+
+            allow publish 172.20.24.0/24;
+            allow play 172.20.24.0/24;
+            deny publish all;
+            deny play all;
           }
         }
       }
