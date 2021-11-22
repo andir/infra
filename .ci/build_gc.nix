d18135a457540333934fa479e2fa55e3c9634310
Andreas Rammhold <andreas@rammhold.de>
2021-11-22 09:17:03 +0100

    ci: deduplicate the sources to avoid build errors

--- .ci/build_gc.nix
+++ .ci/build_gc.nix
@@ -7,6 +7,6 @@ in
 pkgs.runCommand "shell-gc-root" { } ''
   mkdir $out
   ${pkgs.lib.concatMapStringsSep "\n" (n: "ln -s ${n} $out") shell.buildInputs}
-  ${pkgs.lib.concatMapStringsSep "\n" (n: "ln -s ${n} $out") (pkgs.lib.filter (n: builtins.typeOf n != "lambda") (pkgs.lib.attrValues sources))}
+  ${pkgs.lib.concatMapStringsSep "\n" (n: "ln -s ${n} $out") (pkgs.lib.unique (pkgs.lib.filter (n: builtins.typeOf n != "lambda") (pkgs.lib.attrValues sources)))}
   ln -s ${builtins.storePath morph} $out
 ''
