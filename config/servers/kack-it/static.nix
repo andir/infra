5575dfa26285cf001eda1feaec8ceab2fff136de
Andreas Rammhold <andreas@rammhold.de>
2022-01-10 21:38:15 +0100

    kack.it: support serving iPXE files from s.rammhold.de

--- config/servers/kack-it/static.nix
+++ config/servers/kack-it/static.nix
@@ -5,8 +5,33 @@
     "d /var/lib/s.h4ck.space 750 andi nginx -"
   ];
 
+  security.acme = {
+    certs."s.rammhold.de" = {
+      keyType = "rsa4096";
+      extraLegoRunFlags = [
+        # re: https://community.letsencrypt.org/t/production-chain-changes/150739/1
+        # re: https://github.com/ipxe/ipxe/pull/116
+        # re: https://github.com/ipxe/ipxe/pull/112
+        # re: https://lists.ipxe.org/pipermail/ipxe-devel/2020-May/007042.html
+        "--preferred-chain"
+        "ISRG Root X1"
+      ];
+      extraLegoRenewFlags = [
+        # re: https://community.letsencrypt.org/t/production-chain-changes/150739/1
+        # re: https://github.com/ipxe/ipxe/pull/116
+        # re: https://github.com/ipxe/ipxe/pull/112
+        # re: https://lists.ipxe.org/pipermail/ipxe-devel/2020-May/007042.html
+        "--preferred-chain"
+        "ISRG Root X1"
+      ];
+    };
+  };
+
   services.nginx = {
     enable = true;
+    recommendedTlsSettings = true;
+    #sslProtocols = "TLSv1.2";
+    sslCiphers = "DEFAULT:AES256-SHA256";
     virtualHosts."kack.it" = {
       locations."/".extraConfig = ''
         add_header Content-Type text/plain;
